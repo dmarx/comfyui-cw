11468e0dc27da15a9feff459ce931c461f627e51
David Marx <david.marx84@gmail.com>
2023-12-22 11:57:40 -0800

    added simple logging to install_script.sh

--- install_script.sh
+++ install_script.sh
@@ -3,20 +3,28 @@
 # Function to clone and install from a repository
 install_repo() {
     repo_url=$1
+    echo "Cloning repository: $repo_url"
     git clone $repo_url
     repo_name=$(basename $repo_url .git)
 
     cd $repo_name
+    echo "Processing repository: $repo_name"
+
     if [ -f "requirements.txt" ]; then
+        echo "Found requirements.txt in $repo_name, installing dependencies..."
         pip install -r requirements.txt
     fi
 
     if [ -f "install.py" ]; then
+        echo "Found install.py in $repo_name, executing script..."
         python install.py
     fi
 
+    echo "Completed processing $repo_name"
     cd ..
 }
+
+echo "Starting installation process..."
 
 # Read each line in repos.txt and install
 while IFS= read -r repo; do
@@ -24,3 +32,5 @@ while IFS= read -r repo; do
         install_repo $repo
     fi
 done < "/repos.txt"
+
+echo "All repositories processed."
