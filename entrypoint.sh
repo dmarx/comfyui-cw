cdebb3706fffaf604ccb2614e066b101e23aee34
David Marx <david.marx84@gmail.com>
2024-01-02 23:27:11 -0800

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# mount persistent volume
+mkdir /mnt/storage
+mount -t nfs -o vers=4.2,noatime,nodiratime,nolock comfyui-atorage-nfs-mount:/storage /mnt/storage
+
+cd /usr/src/app/ComfyUI
+
+# todo: pass in command line arguments
+python main.py --port 8000 --listen 0.0.0.0
